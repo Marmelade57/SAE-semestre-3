7e7df2fab3c677fc9800706978f12e10b885bf19
HolySheet54 <zain.pam1234@gmail.com>
2024-12-09 23:40:57 +0100

    Ajout des classes Utilisateurs, Roles, Jeux et Contient

--- appli_ludotheque/appli_ludotheque/lib/connexion.dart
+++ appli_ludotheque/appli_ludotheque/lib/connexion.dart
@@ -1,20 +1,13 @@
 import 'package:mysql1/mysql1.dart';
-// import 'package:http/http.dart';
 
-class Connexion{
-  Future<MySqlConnection> connexion() async{
-    final conn = await MySqlConnection.connect(ConnectionSettings(
-      host: 'https://devweb.iutmetz.univ-lorraine.fr/phpmyadmin/',
-      port: 0,
-      user: 'identifiant_appli',
-      password: 'motDePasse',
-      db: 'identifiant_sae301'
+class Connexion {
+  static Future<MySqlConnection> getConnexion() async {
+    return await MySqlConnection.connect(ConnectionSettings(
+      host: '51.38.64.145',
+      port: 3306,
+      user: 'sae',
+      password: '301',
+      db: 'SAE301',
     ));
-
-    final result = await conn.query('SELECT * FROM UTILISATEUR');
-    for (var row in result) {
-      print('id: ${row['id_user']}, nom: ${row['nom_user']}, prénom: ${row['prenom_user']}');
-    }
-    return conn;
   }
 }
---
+++ appli_ludotheque/appli_ludotheque/lib/modele/role.class.dart
@@ -0,0 +1,39 @@
+import '../connexion.dart';
+
+class Role {
+  final int idRole;
+  final String nomRole;
+  final String descRole;
+
+  Role({
+    required this.idRole,
+    required this.nomRole,
+    required this.descRole,
+  });
+
+  factory Role.fromMap(Map<String, dynamic> map) {
+    return Role(
+      idRole: map['id_role'],
+      nomRole: map['nom_role'],
+      descRole: map['desc_role'],
+    );
+  }
+
+  static Future<List<Role>> fetchAll() async {
+    final conn = await Connexion.getConnexion();
+    try {
+      final results = await conn.query('SELECT * FROM ROLES');
+
+      List<Role> roles = results.map((row) {
+        return Role.fromMap(row.fields);
+      }).toList();
+
+      return roles;
+    } catch (e) {
+      print('Erreur lors de la récupération des rôles : $e');
+      return [];
+    } finally {
+      await conn.close();
+    }
+  }
+}
